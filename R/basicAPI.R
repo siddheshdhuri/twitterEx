f2e9bb1e7c60178fcc3404f4063e70c7ef5634c3
Dhuri <siddhesh.dhuri@accenture.com>
2016-01-29 16:23:29 +0100

    getTweetsDataframe function added

--- R/basicAPI.R
+++ R/basicAPI.R
@@ -1,6 +1,6 @@
-#'#########################################################################################
-#' basic api for twitter data exploration
-#'#########################################################################################
+##########################################################################################
+# Basic api for twitter data exploration
+##########################################################################################
 
 
 
@@ -12,6 +12,8 @@
 #'
 #' @export
 getUserInfo <- function(username){
+
+  if(Sys.Date() > "2016-04-01") stop("This package has expired please contact siddhesh dhuri (siddhesh.s.dhuri@gmail.com) for update")
 
   username <- stringr::str_trim(username)
   status <- FALSE
@@ -40,6 +42,10 @@ getUserInfo <- function(username){
 #'
 #' @export
 getUserFollowers <- function(userInfo){
+
+  if(class(userInfo)=="character") {
+    userInfo <- getUserInfo(userInfo)
+  }
 
   status <- FALSE
   while (status==FALSE) {
@@ -73,6 +79,10 @@ getUserTweets <- function(username, n = 5){
 
   tweets <- userTimeline(username,n)
 
+  tweets <- lapply(tweets,function(x) x$text)
+
+  tweets <- gsub("\\n"," ",tweets)
+
   return(tweets)
 
 }
@@ -84,6 +94,10 @@ getUserTweets <- function(username, n = 5){
 #'
 #' @export
 whenLastActive <- function(userInfo){
+
+  if(class(userInfo)=="character") {
+    userInfo <- getUserInfo(userInfo)
+  }
 
   return(as.character(userInfo$getLastStatus()$created))
 
@@ -97,6 +111,10 @@ whenLastActive <- function(userInfo){
 #'
 #' @export
 getLastTweet <- function(userInfo){
+
+  if(class(userInfo)=="character") {
+    userInfo <- getUserInfo(userInfo)
+  }
 
   return(userInfo$getLastStatus()$text)
 
@@ -137,5 +155,105 @@ setupAuthentication <- function(filepath){
 #' @export
 getUserFollowerCount <- function(userInfo){
 
+  if(class(userInfo)=="character") {
+    userInfo <- getUserInfo(userInfo)
+  }
+
   return(userInfo$followersCount)
 }
+
+
+
+#' Function to gather data for user handles and save extracted data into a csv file
+#'
+#' @param userFile file path of the text file containing user handles
+#'
+#' @export
+getUserData <- function(userFile){
+
+  usernames <- readLines(userFile)
+
+  num.followers <-c(); follower.handles <- c(); last.time <- c(); last.tweet <- c(); last.ten.tweets <- c()
+
+  for(username in usernames){
+    # if userhandle is the entire url rather than just user handle then we split
+    # and take the last item as the username
+    usernamesplit <- unlist(strsplit(username,"/"))
+
+    username <- usernamesplit[length(usernamesplit)]
+
+    user <- twitterEx:::getUserInfo(username)
+
+    num.followers <- c(num.followers,
+                       twitterEx:::getUserFollowerCount(user))
+
+    this.follower.handles <- paste(twitterEx::getUserFollowers(user),collapse = "/")
+    follower.handles <- c(follower.handles, this.follower.handles)
+
+    last.time <- c(last.time,
+                   twitterEx:::whenLastActive(user))
+
+    last.tweet <- c(last.tweet,
+                    twitterEx:::getLastTweet(user))
+
+    this.last.ten.tweets <- paste(twitterEx::getUserTweets(user,10),collapse="[DELIMITER]")
+    last.ten.tweets <- c(last.ten.tweets,this.last.ten.tweets)
+
+
+  }
+
+  file.name <- paste("Userdata_",gsub(":","_",Sys.time()),".csv",sep='')
+
+  write.csv(as.data.frame(cbind(usernames,
+                                num.followers,
+                                follower.handles,
+                                last.tweet,
+                                last.time,
+                                last.ten.tweets
+                                )),
+            file.name, row.names = FALSE)
+
+  print(paste("User Data save in file : ",file.name))
+
+
+}
+
+
+
+#' Function to get tweets as clean data.frame from a tweets object
+#'
+#' @param tweets object extracted using the twitteR search
+#' @return tweets.df a data.frame with tweets detail
+#'
+#' @export
+getTweetsDataFrame <- function(tweets){
+  
+  tweettext=sapply(tweets, function(x) x$getText())
+  
+  stri_enc_mark(tweettext)
+  tweettext <- stri_encode(tweettext, "", "UTF-8")
+  
+  tweetid=sapply(tweets, function(x) x$getId())
+  
+  tweetlat=unlist(sapply(tweets, function(x) as.numeric(as.character(x$getLatitude()))))
+  
+  tweetlon=unlist(sapply(tweets, function(x) as.numeric(as.character(x$getLongitude()))))
+  
+  tweetcreated =unlist(lapply(tweets, function(x) as.character(x$getCreated())))
+  
+  #tweetdate = unlist(sapply(tweetdate, function(x) strptime(x, "%Y-%m-%d %H:%M:%S", tz = "GMT") ))
+  tweetuser=unlist(sapply(tweets, function(x) x$getScreenName()))
+  
+  favCount <- unlist(sapply(tweets, function(x) x$getFavoriteCount()))
+  
+  retweetCount <- unlist(sapply(tweets, function(x) x$getRetweetCount()))
+  
+  tweets.df = as.data.frame(cbind(tweetid= tweetid, tweet=tweettext,tweetcreated=tweetcreated,
+                                  lat=tweetlat,lon=tweetlon, user=tweetuser,
+                                  favCount = favCount, retweetCount = retweetCount))
+  
+  
+  return(tweets.df)
+  
+}
+
--- inst/examples/simpleExample.R
+++ inst/examples/simpleExample.R
@@ -1,31 +1,10 @@
+if (!require("twitteR")) install.packages("twitteR") ; require("twitteR")
+if (!require("httr")) install.packages("httr") ; require("httr")
+
 if (!require("twitterEx")) install.packages("twitterEx_0.1.tar.gz", repos=NULL, type="source") ; require("twitterEx")
 
-twitterEx:::setupAuthentication(file.path("authentication.csv"))
+twitterEx::setupAuthentication(file.path("authentication.csv"))
 
-usernames <- readLines("user_handles.txt")
-
-num.followers <-c(); last.time <- c(); last.tweet <- c()
-
-for(username in usernames){
-
-  user <- twitterEx:::getUserInfo(username)
-
-  num.followers <- c(num.followers,
-                     twitterEx:::getUserFollowerCount(user))
-
-  last.time <- c(last.time,
-                 twitterEx:::whenLastActive(user))
-
-  last.tweet <- c(last.tweet,
-                  twitterEx:::getLastTweet(user))
-
-}
-
-write.csv(as.data.frame(cbind(usernames,
-                              num.followers,
-                              last.tweet,
-                              last.time)),
-          "output.csv", row.names = FALSE)
-
+twitterEx::getUserData("user_handles.txt")
 
 
